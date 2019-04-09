5dc5aedecf261dc2f8e8e3f65416b35c6fa3856e
Tina Kurian <tkurian@redhat.com>
2019-04-09 14:41:32 -0400

    renaming codebase to gitsourceref (#11)

--- pkg/apis/devconsole/v1alpha1/component_types.go
+++ pkg/apis/devconsole/v1alpha1/component_types.go
@@ -4,16 +4,16 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// ComponentSpec defines the desired state of Component
+// ComponentSpec defines the desired state of Component.
 // +k8s:openapi-gen=true
 type ComponentSpec struct {
 	// Container image use to build (nodejs, golang etc..)
 	BuildType string `json:"buildType"`
-	// Codebase is the source code of your component. Atm only public remote URL are supported.
-	Codebase string `json:"codebase"`
+	// GitSourceRef is the source code of your component. Currently, only public remote URLs are supported.
+	GitSourceRef string `json:"codebase"`
 }
 
-// ComponentStatus defines the observed state of Component
+// ComponentStatus defines the observed state of Component.
 // +k8s:openapi-gen=true
 type ComponentStatus struct {
 	RevNumber string
@@ -21,7 +21,7 @@ type ComponentStatus struct {
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// Component is the Schema for the components API
+// Component is the Schema for the components API.
 // +k8s:openapi-gen=true
 type Component struct {
 	metav1.TypeMeta   `json:",inline"`
@@ -38,7 +38,7 @@ func (c *Component) GetName() string {
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// ComponentList contains a list of Component
+// ComponentList contains a list of Component.
 type ComponentList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
